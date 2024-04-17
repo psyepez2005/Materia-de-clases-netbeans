eb09f097fcdbec155630b935977d085750a7a473
PedroYep <yepedro00@gmail.com>
2024-04-17 10:45:44 -0500

    2

---
+++ estDatClas2/src/estdatclas2/EstDatClas2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package estdatclas2;
+
+/**
+ *
+ * @author utpl
+ */
+public class EstDatClas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
--- practicaArchivos/src/practicaarchivos/Act_en_clase1.java
+++ practicaArchivos/src/practicaarchivos/Act_en_clase1.java
@@ -1,10 +1,13 @@
 package practicaarchivos;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+
 public class Act_en_clase1 {
+
     public static void main(String[] args) {
         try {
             Scanner put = new Scanner(System.in);
@@ -16,19 +19,23 @@ public class Act_en_clase1 {
                 System.out.println("El archivo ya existe");
                 System.out.println("Ubicado en: " + actClase1.getPath());
             }
-            FileWriter fw = new FileWriter("C:\\\\Users\\\\utpl\\\\Desktop\\\\actClase1.txt",true);
+            FileWriter fw = new FileWriter("C:\\\\Users\\\\utpl\\\\Desktop\\\\actClase1.txt", true);
             for (int i = 0; i < 5; i++) {
-                System.out.println("Introduzca el nombre "+(i+1));
+                System.out.println("Introduzca el nombre " + (i + 1));
                 String nombres = put.next();
-                fw.write("Nombre"+(i+1)+": "+nombres+"  -  ");
-                System.out.println("Introduzca la edad "+(i+1));
+                fw.write("Nombre" + (i + 1) + ": " + nombres + "  -  ");
+                System.out.println("Introduzca la edad " + (i + 1));
                 String edades = put.next();
-                fw.write("Edad"+(i+1)+": "+edades+"\n");
+                fw.write("Edad" + (i + 1) + ": " + edades + "\n");
             }
             fw.close();
+            FileReader lector = new FileReader("C:\\\\Users\\\\utpl\\\\Desktop\\\\actClase1.txt");
+            int i;
+            while ((i = lector.read()) != -1) {
+                System.out.print((char) i);
+            }
         } catch (IOException e) {
         }
-        
-        
+
     }
 }
