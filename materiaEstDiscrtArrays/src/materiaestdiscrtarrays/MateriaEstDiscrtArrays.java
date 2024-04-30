7e1f91977d0171e475cf09fd9cd7c9eb8e1e8326
Pedro <yepedro00@gmail.com>
2024-04-30 17:33:18 -0500

    a

--- materiaEstDiscrtArrays/src/materiaestdiscrtarrays/MateriaEstDiscrtArrays.java
+++ materiaEstDiscrtArrays/src/materiaestdiscrtarrays/MateriaEstDiscrtArrays.java
@@ -1,6 +1,7 @@
 
 package materiaestdiscrtarrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MateriaEstDiscrtArrays {
@@ -10,40 +11,66 @@ public class MateriaEstDiscrtArrays {
         int a[] = new int[10];
         
         /*//////////////////////
-        Scanner put = new Scanner(System.in);
+        Scanner put = new Scanner(System.in); //esto no lo uso xq es para llenar por consola, en vez de eso uso la funcion llenar
         for (int i = 0; i < 10; i++) {
             a[i]=put.nextInt();
         }
         ////////////////////////*/
         
         llenar(a);
-        
-        
         System.out.println(presentar(a));
 
         ///////////////////////
+        
         int numBuscar=7;
         System.out.println(buscar(a,numBuscar));
         
+        /*//////////////////////
         
         int indiceElim=buscar(a,numBuscar);
         eliminar( a,  indiceElim);
-        
-        
-
         System.out.println(presentar(a));
+        
+        //////////////////////*//*
 
+        ordenar(a);
+        System.out.println(presentar(a));
+        
+        //////////////////////*/
         
     }
-    public static int buscar(int a[], int numBuscar){
-        int b[] = new int[a.length];
-        for (int i = 0; i < 10; i++) {
-            if(a[i]==numBuscar){
-                
-                return i;
-            }
+    public static String buscar(int a[], int numBuscar){
+        //int b[] = new int[a.length];
+        //int k=0;
+        String y="";
+        /*
+        for (int i = 0; i < b.length; i++) {
+            b[i]=0;
         }
-        return -1;
+        */
+        for (int i = 0; i < a.length-1; i++) {
+            if(a[i]==numBuscar){
+                y+=i;
+                /*
+                b[k]=i;
+                k++;
+                */
+                /*
+                if(i == a.length){
+                    return y;
+                }
+                */
+                return y;
+                
+            }
+            
+        }
+        /*
+        for (int i = 0; i < b.length; i++) {
+            b[i]=-1;
+        }
+        */
+        return "-1";
     }
     public static void eliminar(int a[], int indiceElim){
         
--- materiaEstDiscrtArrays/src/materiaestdiscrtarrays/arrays.java
+++ materiaEstDiscrtArrays/src/materiaestdiscrtarrays/arrays.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package materiaestdiscrtarrays;
-
-/**
- *
- * @author utpl
- */
 public class arrays {
-    
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        llenar(a);
+        System.out.println(presentar(a));
+        int insertar = 99;
+        int indice = 4;
+        insertar(a,insertar,indice);
+        System.out.println("/////////////////////////");
+        System.out.println(presentar(a));
+    }
+    public static void insertar(int a[], int insertar, int indice){
+        
+        if(indice!=-1){
+            for (int i = 1; i < (a.length-indice); i++) {
+                a[a.length-i]=a[a.length-i-1];
+            }
+            
+        }
+        a[indice]=insertar;
+
+    }
+    public static void llenar(int a[]){
+        for (int i = 0; i < a.length; i++) {
+            int random=(int)(Math.random()*(0-10)+10);
+            a[i]=random;
+        }
+    }
+    public static String presentar(int a[]){
+        String array="\n================\n";
+        for (int i = 0; i < a.length; i++) {
+            array+=(a[i]+" - ");
+        }
+        array+="\n================\n";
+        return array;
+    }
 }
